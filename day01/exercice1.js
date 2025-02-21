9a55666ebf70112e7b7d5757f2676bc6906cbcfc
Salma-007 <salmaelallali11@gmail.com>
2025-02-21 12:26:40 +0100

    exercice 1 avancement

---
+++ day01/exercice1.js
@@ -0,0 +1,40 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+]
+  
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+]
+
+// declaring an empty array
+let arrayDeclaration = [];
+
+let array1 = [1, 2, 3, 5, 60];
+
+let longueur = array1.length;
+
+let first = array1[0];
+
+let last = array1[array1.length-1];
+
+let middle = array[Math.round(array1.length/2)];
+
+let mixedDataTypes = [1, 'salma', 11.11, 'taha', 120];
+
+let itCompanies = []
